633c80db74bb4c2741ef1e21c06babc1db50d7cc
vadim-ivlev <vadim.ivlev@gmail.com>
2020-10-27 11:03:12 +0300

    .

--- sh/build-deploy-directory.sh
+++ sh/build-deploy-directory.sh
@@ -2,6 +2,11 @@
 
 rm -rf deploy/docker-compose.yml
 rm -rf deploy/README.md
+rm -rf deploy/sh
+
 
 cp -R README.md deploy/
 cp -R docker-compose.yml deploy/
+mkdir -p deploy/sh
+cp sh/dump.sh deploy/sh/
+cp sh/restore.sh deploy/sh/
