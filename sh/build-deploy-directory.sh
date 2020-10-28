85fb4738c50cb83e7e4dd9294775e7ecff7b5cd0
vadim-ivlev <vadim.ivlev@gmail.com>
2020-10-28 22:04:42 +0300

    .

--- sh/build-deploy-directory.sh
+++ sh/build-deploy-directory.sh
@@ -3,7 +3,7 @@
 rm -rf deploy/docker-compose.yml
 rm -rf deploy/README.md
 rm -rf deploy/notes.md
-rm -rf deploy/notebooks
+# rm -rf deploy/notebooks
 rm -rf deploy/Dockerfile-notebook
 
 rm -rf deploy/sh
@@ -12,7 +12,7 @@ rm -rf deploy/sh
 cp -R docker-compose.yml deploy/
 cp -R README.md deploy/
 cp -R notes.md deploy/
-cp -R notebooks deploy/
+# cp -R notebooks deploy/
 cp -R Dockerfile-notebook deploy/
 
 mkdir -p deploy/sh
